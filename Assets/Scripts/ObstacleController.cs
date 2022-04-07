ccab96d71a764dda5d34627b71a87f014f1ae00a
Pavel Demin <paveldemin@me.com>
2022-04-07 13:13:51 +0800

    Add real GameOver logic

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -7,7 +7,7 @@ public class ObstacleController : MonoBehaviour
 {
     public int ObstacleHP = 10;
     
-    private TextMeshProUGUI HealthText;    
+    private TextMeshProUGUI HealthText;
 
     private void OnCollisionEnter(Collision other) {
         if (other.collider.tag == "Projectile") {
@@ -38,6 +38,6 @@ public class ObstacleController : MonoBehaviour
 
     private void GameOver(GameObject PlayerObject) {
         Destroy(PlayerObject);
-        // some UI logic here as well
+        GameController.GameOver();
     }
 }

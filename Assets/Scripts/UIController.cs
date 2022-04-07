94b2d14ee28f90d0626d46176e9bd3feebce2ace
Pavel Demin <paveldemin@me.com>
2022-04-07 13:13:29 +0800

    Update UI controller
    Make sure that Damage Valie UI is updated from DataContainer

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -4,12 +4,19 @@ using TMPro;
 public class UIController : MonoBehaviour {
     
     private TextMeshProUGUI ScoreValueUI;
+    private TextMeshProUGUI DamageValueUI;
 
     private void Start() {
-        ScoreValueUI = GameObject.FindGameObjectWithTag("ScoreValue").GetComponent<TextMeshProUGUI>();
+        ScoreValueUI = GameObject
+            .FindGameObjectWithTag("ScoreValue")
+            .GetComponent<TextMeshProUGUI>();
+        DamageValueUI = GameObject
+            .FindGameObjectWithTag("DamageValue")
+            .GetComponent<TextMeshProUGUI>();
     }
 
     private void Update() {
         ScoreValueUI.text = DataContainer.Score.ToString();
+        DamageValueUI.text = DataContainer.Damage.ToString();
     }
 }

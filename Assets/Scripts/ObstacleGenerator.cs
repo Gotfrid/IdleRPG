47fe01c5bf310681272c2f33153985b3bc00ae95
Pavel Demin <paveldemin@me.com>
2022-04-07 13:47:27 +0800

    Generate Bonuses

--- Assets/Scripts/ObstacleGenerator.cs
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -7,6 +7,14 @@ public class ObstacleGenerator : MonoBehaviour
   [SerializeField] private GameObject Obstacle;
   [SerializeField] private float DistanceToNextObstacle = 50f;
 
+  /*
+  Not the best bonus system: attach three different bonuses
+  and based on a random integer select the bonus to generate
+  */
+  [SerializeField] private GameObject BonusDamage;
+  [SerializeField] private GameObject BonusSpeed;
+  [SerializeField] private GameObject BonusUnit;
+
   private GameObject LastObstacleOne;
   private GameObject LastObstacleTwo;
   private int NumberOfRows = 0;
@@ -21,6 +29,12 @@ public class ObstacleGenerator : MonoBehaviour
       Destroy(LastObstacleTwo, 5f);
       LastObstacleOne = GenerateObstacle(-2.5f);
       LastObstacleTwo = GenerateObstacle(2.5f);
+
+      // Only generate a bonus after every second row of obstacles
+      if (NumberOfRows % 2 == 0)
+      {
+        GenerateBonus(Random.Range(0, 2));
+      }
     }
   }
 
@@ -32,6 +46,33 @@ public class ObstacleGenerator : MonoBehaviour
         Quaternion.identity
     );
     return obstacle;
+  }
+
+  private void GenerateBonus(int BonusType)
+  {
+    switch (BonusType)
+    {
+      case 0:
+        InstantiateBonus(BonusDamage);
+        break;
+      case 1:
+        InstantiateBonus(BonusSpeed);
+        break;
+      case 2:
+        InstantiateBonus(BonusUnit);
+        break;
+    }
+
+
+  }
+
+  private void InstantiateBonus(GameObject BonusObject)
+  {
+    Instantiate(
+      BonusObject,
+      new Vector3(0, 2f, Player.position.z + DistanceToNextObstacle * 1.5f),
+      Quaternion.identity
+    );
   }
 
 }
